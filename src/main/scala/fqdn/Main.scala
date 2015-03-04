55dc78755862ed781738bf1054764fad0f165743
Alex Henning Johannessen <ahjohannessen@gmail.com>
2015-03-04 15:55:37 +0000

    Update Main.scala

--- src/main/scala/fqdn/Main.scala
+++ src/main/scala/fqdn/Main.scala
@@ -2,14 +2,8 @@ import scala.reflect.runtime.universe._
 
 object Main extends App {
 
-  // 1) With sbt run we get: 
-  //    `Int => scala.Option[Int]`
-
-  // 2) With sbt-native-packager stage script we get:
-  //	`Int => Option[Int]`
-
   println {
-    typeTag[Int => Option[Int]].tpe.toString
+    typeTag[Option[Int]].tpe.toString
   }
 
 }
